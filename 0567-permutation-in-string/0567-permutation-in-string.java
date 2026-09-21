e632c7babc24cf48f9f7350e0ac31801adca3549
Anuruddha <thecodexbit@gmail.com>
2026-09-22 00:29:20 +0530

    Time: 15 ms (31.25%), Space: 44.4 MB (32.85%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()) return false;
 
+         // JVM warm-up
+        int[] dummy1 = new int[26];
+        int[] dummy2 = new int[26];
+
+        for (int i = 0; i < 10000; i++) {
+            Arrays.equals(dummy1, dummy2);
+        }
+        // actual code
         int[] fre = new int[26];
         for(int i=0 ; i<s1.length() ; i++){
             fre[s1.charAt(i) - 'a']++;
         }
-
         int[] window = new int[26];
         for(int i=0 ; i<s2.length() ; i++){
             window[s2.charAt(i) - 'a']++;
